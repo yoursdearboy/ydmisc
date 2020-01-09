9f381c84e1db7a3bfbeb6bbc22afce690be3b0a2
Kirill Voronin <kirill.voronin@fccho-moscow.ru>
2020-01-09 18:15:54 +0300

    Added ggplot before after scale

--- ggplot/scales.R
+++ ggplot/scales.R
@@ -1,3 +1,7 @@
+scale_x_before_after <- function(...) {
+  scale_x_discrete(limits = c('before', 'after'), labels = c('До', 'После'), name = NULL, ...)
+}
+
 scale_y_density <- function(...) {
   scale_y_continuous(expand = c(0,0,.05,0), name = 'Density', ...)
 }
